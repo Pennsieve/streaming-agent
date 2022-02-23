fdd45b476abc330ad0c727fbd1734010eeae9d68
jasperhuang <jashuang29@gmail.com>
2022-02-23 14:53:07 -0500

    initial commit
    server.py -- flask methods
    echo-server.py -- sample web socket echo server
    installation-notes.txt -- how to pip install with venv
    venv folder -- virtual environment

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import request
+import json
+import uuid
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+# request format:
+# -- "format": label or identifier for a data format or ingest pre-processing -- this is the transformation piece which knows specific data formats (e.g., EDF) and converts to standard format
+# -- "stream-identifier": label or identifier [optional?] -- short string that will uniquely identify the stream; user can remember it and use it later
+# -- "description":  long string, a sentence or two describing the data
+# -- "tags" [optional]: list of tags to enable searching and filtering
+# 
+# 
+@app.route('/publish', methods=['POST'])
+def publish():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        req_dict = json.loads(request.json)
+
+        my_uuid = uuid.uuid4()
+
+        # write format, stream-identifier, description, tags, and my_uuid to csv
+
+        return {
+          "uuid": my_uuid,
+          "url": "dummy-url"
+        }
+    else:
+        return 'Content-Type not supported!'
