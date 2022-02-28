c1518aa2c5a18993f9e7bdb79b68277fa598eb72
jasperhuang <jashuang29@gmail.com>
2022-02-28 11:40:53 -0500

    write stream metadata to csv

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 import json
 import uuid
+import csv
 
 app = Flask(__name__)
 
@@ -25,6 +26,19 @@ def publish():
         my_uuid = uuid.uuid4()
 
         # write format, stream-identifier, description, tags, and my_uuid to csv
+        csv_columns = ['uuid', 'format', 'stream-identifier', 'description', 'tags']
+        req_dict['uuid'] = my_uuid
+
+        file_name = "stream-metadata-" + my_uuid + ".csv"
+        try:
+            with open(file_name, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                writer.writeheader()
+                writer.writerow(req_dict)
+        except IOError:
+            print("I/O error")
+
+        # instantiate web socket connection
 
         return {
           "uuid": my_uuid,
