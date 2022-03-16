97d7f438d1916e4076b5013d7b09d9677c9d400f
jasperhuang <jashuang29@gmail.com>
2022-03-16 12:43:28 -0400

    tests

---
+++ tests/test_server.py
@@ -0,0 +1,41 @@
+import sys
+import os
+myDir = os.getcwd()
+sys.path.append(myDir)
+
+from server import app
+from flask import json
+
+import pytest
+
+def test_hello_world():
+    response = app.test_client().get('/')
+
+    print("======")
+    print(response)
+    print("======")
+
+    # data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 200
+    # print(data)
+    # assert response.data == "<p>Hello, World!</p>"
+
+def test_publish():        
+    response = app.test_client().post(
+        '/publish',
+        data=json.dumps(
+          {
+            'format':'edf',
+            'stream-identifier': 'test-id',
+            'description': 'this is a test stream',
+            'tags': ['tag1', 'tag2']
+          }
+        ),
+        content_type='application/json',
+    )
+
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 200
+    assert data['url'] == 'dummy-url'
---
+++ tests/test_server_2.py
@@ -0,0 +1,31 @@
+import unittest
+
+from server import app
+import json
+
+class BasicTestCase(unittest.TestCase):
+    def test_home(self):
+      tester = app.test_client(self)
+      response = tester.post(
+        '/publish',
+        data=json.dumps(
+          {
+            'format':'edf',
+            'stream-identifier': 'test-id',
+            'description': 'this is a test stream',
+            'tags': ['tag1', 'tag2']
+          }
+        ),
+        content_type='application/json'
+      )
+      self.assertEqual(response.status_code, 200)
+      self.assertEqual(response.data, b'Hello World!')
+      
+    # def test_other(self):
+    #     tester = app.test_client(self)
+    #     response = tester.get('a', content_type='html/text')
+    #     self.assertEqual(response.status_code, 404)
+    #     self.assertTrue(b'does not exist' in response.data)
+        
+if __name__ == '__main__':
+    unittest.main()
