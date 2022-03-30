58752284a3d859902b6bdf4e95aa5a881d42f4f9
jasperhuang <jashuang29@gmail.com>
2022-03-30 14:52:40 -0400

    Update test_server.py

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,10 +1,13 @@
 import sys
 import os
+from time import sleep
 myDir = os.getcwd()
 sys.path.append(myDir)
 
 from server import app
 from flask import json
+
+import websocket.websocket_functions as websocket_functions
 
 import pytest
 
@@ -20,23 +23,31 @@ def test_hello_world():
     assert response.status_code == 200
     assert data == "<p>Hello, World!</p>"
 
-def test_publish():        
-    response = app.test_client().post(
-        '/publish',
-        data=json.dumps(
-          {
-            'format':'edf',
-            'stream-identifier': 'test-id',
-            'description': 'this is a test stream',
-            'tags': ['tag1', 'tag2']
-          }
-        ),
-        content_type='application/json',
-    )
+def test_publish():
+    with app.test_client() as c:
+      response = c.post(
+          '/publish',
+          data=json.dumps(
+            {
+              'format':'edf',
+              'stream-identifier': 'test-id',
+              'description': 'this is a test stream',
+              'tags': ['tag1', 'tag2']
+            }
+          ),
+          content_type='application/json',
+      )
 
-    assert response is not None
+      sleep(10)
+      assert websocket_functions.is_port_in_use(8765)
+      # print(websocket_functions.is_port_in_use(8766))
+      # print(websocket_functions.is_port_in_use(8767))
 
-    data = json.loads(response.get_data(as_text=True))
 
-    assert response.status_code == 200
-    assert data['url'] == 'dummy-url'
+
+      assert response is not None
+
+      data = json.loads(response.get_data(as_text=True))
+
+      assert response.status_code == 200
+      assert data['url'] == 'dummy-url'
